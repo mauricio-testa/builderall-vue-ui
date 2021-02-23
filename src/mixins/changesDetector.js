d66520b6d6004f9b43e692df5d0bb0c608c57408
mauricio-testa <iciotesta@hotmail.com>
2021-02-23 10:15:59 -0300

    add mixin & utils

---
+++ src/mixins/changesDetector.js
@@ -0,0 +1,18 @@
+const mixinChangesDetector = {
+  data: () => {
+    return {
+      initialObject: {}
+    }
+  },
+
+  methods: {
+    $_mixinChangesDetector_storeInitial(object) {
+      this.initialObject = JSON.stringify(object);
+    },
+    $_mixinChangesDetector_hasChanged(newObject) {
+      return JSON.stringify(newObject) !== this.initialObject
+    }
+  }
+}
+
+export default mixinChangesDetector
---
+++ src/utils/index.js
@@ -0,0 +1,18 @@
+export const copyToClipboard = text => {
+  const tempInput = document.createElement("input");
+  tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+  tempInput.value = text;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+}
+
+export const previewUpload = data => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(data);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  })
+}
