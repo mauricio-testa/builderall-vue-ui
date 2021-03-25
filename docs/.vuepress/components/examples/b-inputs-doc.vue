5584ca0ce984bdb5c3b33bd6e44f87ea2e45b94c
mauricio-testa <iciotesta@hotmail.com>
2021-03-25 12:02:04 -0300

    customize datetime input

--- docs/.vuepress/components/examples/b-inputs-doc.vue
+++ docs/.vuepress/components/examples/b-inputs-doc.vue
@@ -1,5 +1,7 @@
 <template>
   <b-card>
+
+    <!-- Text inputs -->
     <bui-form-group
       label="Label with help"
       help="Help text"
@@ -11,7 +13,7 @@
     </bui-form-group>
 
     <bui-form-group
-      label="Label with error"
+      label="Input error"
       error="error text"
     >
       <b-form-input
@@ -47,19 +49,16 @@
       <b-form-input type="search" />
     </bui-form-group>
 
-    <bui-form-group label="Select">
-      <b-form-select :options="['one', 'two']" />
+    <bui-form-group class="my-3" label="Big input">
+      <b-input
+        size="xl"
+        placeholder="aksdj"
+      />
     </bui-form-group>
 
-    <bui-form-group
-      label="Radios"
-      help="I need help"
-      required
-    >
-      <b-form-radio-group
-        :options="['one', 'two']"
-        name="radio-options"
-      />
+    <!-- Select -->
+    <bui-form-group label="Select">
+      <b-form-select :options="['one', 'two']" />
     </bui-form-group>
 
     <bui-form-group
@@ -71,6 +70,7 @@
       <b-form-select :options="['one', 'two']" />
     </bui-form-group>
 
+    <!-- Checkbox -->
     <bui-form-group
       label="Switches"
       required
@@ -89,6 +89,10 @@
       >
         <label> Switch </label>
       </b-form-checkbox>
+      
+    </bui-form-group>
+
+    <bui-form-group>
       <b-form-checkbox
         class="bui-checkbox-rounded"
         :checked="true"
@@ -99,7 +103,7 @@
 
     <b-form-checkbox
       id="boxed"
-      class="bui-checkbox-boxed bui-checkbox-rounded"
+      class="bui-checkbox-boxed bui-checkbox-rounded my-3"
       size="lg"
       :checked="true"
     >
@@ -111,10 +115,15 @@
       />
     </b-form-checkbox>
 
-    <bui-form-group class="my-3" label="My website name is">
-      <b-input
-        size="xl"
-        placeholder="aksdj"
+    <!-- radio -->
+    <bui-form-group
+      label="Radios"
+      help="I need help"
+      required
+    >
+      <b-form-radio-group
+        :options="['one', 'two']"
+        name="radio-options"
       />
     </bui-form-group>
 
@@ -123,6 +132,15 @@
         :options="['Radio 1','Radio 2']"
         class="bui-radiogroup-box"
       />
+    </bui-form-group>
+
+    <!-- date time picker -->
+    <bui-form-group label="Date">
+      <b-form-datepicker/>
+    </bui-form-group>
+
+    <bui-form-group label="Hour">
+      <b-form-timepicker minutes-step="5"/>
     </bui-form-group>
   </b-card>
 </template>
